8bf2a2e663b8206f16e682073c21e3e0ceb264c2
HauHet.Co <63187237+HauHetCo@users.noreply.github.com>
2025-02-18 02:13:42 +0200

    Update logic.js

--- USGS Map/static/js/logic.js
+++ USGS Map/static/js/logic.js
@@ -1,2 +1,8 @@
 // Add console.log to check to see if our code is working.
 // console.log("working");
+
+// // Store our JSON API endpoint as queryUrl.
+// let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// // Retrieve the earthquake GeoJSON data, // // Perform a GET request to the query URL/
+// // Once we get a response, send the data.features object to the createFeatures function.
+// d3.json(queryUrl).then(function(data){L.geoJson(data).addTo(myMap)});

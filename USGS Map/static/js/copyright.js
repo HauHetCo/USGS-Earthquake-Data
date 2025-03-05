805054df1a398006bda807f2e57329b33d5c0554
HauHet.Co <63187237+HauHetCo@users.noreply.github.com>
2025-03-05 20:40:39 +0200

    Add files via upload

---
+++ USGS Map/static/js/copyright.js
@@ -0,0 +1,18 @@
+/*
+
+HauHet plc.
+hauhet.co
+
+We are HauHet
+AI Energy Sector
+Advanced Distribution Management System
+
+Headquarters
+HauHet plc. – Xolo Go OU
+Private Limited Company
+
+Kalasadama 4
+10415 – Tallinn
+Estonia
+
+*/
